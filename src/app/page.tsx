5406d18e5202b6442c87916080d1f076133b221f
frontenchi <frontendresearch@gmail.com>
2025-01-22 23:45:28 +0300

    testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/icons";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import Phone from "@/components/phone";
 import { Check, Star } from "lucide-react";
@@ -67,6 +68,80 @@ export default function Home() {
               <img className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block " src="/your-image.png" alt="" />
               <img className="absolute w-20 -left-6 -bottom-6 select-none" src="/line.png" alt="" />
               <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900 " >
+              What our <span className="relative px-2">customers{' '}<Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500"/></span> say
+            </h2>
+
+            <img className="w-24 order-0 lg:order-2" src="/snake-2.png" alt="" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+              </div>
+
+              {/* first testimonials */}
+              <div className="text-lg leading-8 ">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, repellat. Omnis alias velit nulla eaque in incidunt necessitatibus. Odio blanditiis incidunt odit, perferendis illo voluptas deserunt at. Voluptas, fugiat reiciendis?
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full h-14 w-14 object-cover" src="/users/user-1.png"/>
+                <div className="flex flex-col ">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600 ">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            {/* second user review */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+              </div>
+
+              {/* first testimonials */}
+              <div className="text-lg leading-8 ">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, repellat. Omnis alias velit nulla eaque in incidunt necessitatibus. Odio blanditiis incidunt odit, perferendis illo voluptas deserunt at. Voluptas, fugiat reiciendis?
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full h-14 w-14 object-cover" src="/users/user-4.jpg"/>
+                <div className="flex flex-col ">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600 ">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </MaxWidthWrapper>
