874236fcd2b8fb9ce747c4db5bccbdf68df5e878
frontenchi <frontendresearch@gmail.com>
2025-01-24 23:27:03 +0300

    reviews section & imgs animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/icons";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import Phone from "@/components/phone";
+import Reviews from "@/components/reviews";
 import { Check, Star } from "lucide-react";
 
 export default function Home() {
@@ -114,7 +115,7 @@ export default function Home() {
 
             </div>
 
-            {/* second user review */}
+            {/* second testimonials */}
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-.5 mb-2">
                 <Star className="h-5 w-5 text-green-600 fill-green-600"/>
@@ -124,7 +125,6 @@ export default function Home() {
                 <Star className="h-5 w-5 text-green-600 fill-green-600"/>
               </div>
 
-              {/* first testimonials */}
               <div className="text-lg leading-8 ">
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, repellat. Omnis alias velit nulla eaque in incidunt necessitatibus. Odio blanditiis incidunt odit, perferendis illo voluptas deserunt at. Voluptas, fugiat reiciendis?
@@ -144,6 +144,55 @@ export default function Home() {
 
             </div>
           </div>
+        </MaxWidthWrapper>
+
+        {/* reviews section */}
+        <div className="pt-16">
+          <Reviews />
+        </div>
+        
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center ">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900 " >
+                Upload your phote and get <span className="bg-green-600 text-white px-2">your own case{' '}</span> now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img src="/arrow.png" alt="" className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0" />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl ">
+                <img src="/horse.jpg" alt="" className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full" />
+              </div>
+
+              <Phone className="w-60" imgSrc="/horse_phone.jpg"/>
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check  className="h-5 w-5 text-green-600 inline mr-1.5"/>
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check  className="h-5 w-5 text-green-600 inline mr-1.5"/>
+              Scratch and fingerprint resitant coating
+            </li>
+            <li className="w-fit">
+              <Check  className="h-5 w-5 text-green-600 inline mr-1.5"/>
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check  className="h-5 w-5 text-green-600 inline mr-1.5"/>
+              5 year print warranty
+            </li>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,7 +59,26 @@ const config: Config = {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
+  		},
+			keyframes: {
+				'fade-in': {
+					from: {
+						opacity: '0'
+					},
+					to: {
+						opacity: '1'
+					}
+				},
+				marquee: {
+					'100%' : {
+						transform: 'translateY(-50%)'
+					} 
+				}
+			},
+			animation: {
+				'marquee': 'marquee var(--marquee-duration) linear infinite',
+				'fade-in': 'fade-in 0.5s linear forwards'
+			}
   	}
   },
   plugins: [require("tailwindcss-animate")],
