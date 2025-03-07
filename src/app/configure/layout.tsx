aa52b58088755158b8c7047bb544e995dbf9550b
frontenchi <frontendresearch@gmail.com>
2025-03-07 03:14:15 +0300

    steps bar design

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from '@/components/max-width-wrapper'
+import Steps from '@/components/steps'
 import React, { ReactNode } from 'react'
 
 export default function Layout({children}: {children: ReactNode}) {
   return (
     <MaxWidthWrapper className='flex-1 flex flex-col'>
+      <Steps />
       {children}
     </MaxWidthWrapper>
   )
